b499677cce08e990551d61548d4caca818ee5159
K-KY <kyuyoungk@naver.com>
2025-04-18 11:39:12 +0900

    Add files via upload

---
+++ form.php
@@ -0,0 +1,25 @@
+<H2>FORM.PHP</H2>
+<?php
+echo "<h2>제출된 이름: {$_POST["name"]}</h2>";
+?>
+<?php
+echo "<h2>제출된 비밀번호: {$_POST["password"]}</h2>";
+?>
+<?php
+echo "<h2>제출된 비밀번호 확인: {$_POST["password-confirm"]}</h2>";
+?>
+<?php
+echo "<h2>제출된 이름: {$_POST["name"]}</h2>";
+?>
+<?php
+echo "<h2>제출된 이메일: {$_POST["email"]}</h2>";
+?>
+<?php
+echo "<h2>제출된 이메일 수신 여부: {$_POST["email_agree"]}</h2>";
+?>
+<?php
+echo "<h2>제출된 전화번호: {$_POST["tel"]}</h2>";
+?>
+<?php
+echo "<h2>제출된 sms 수신 여부: {$_POST["sms_agree"]}</h2>";
+?>
